eac4ae9c9b75a2bf1ef73b68c5f8dea927fac51d
quzard <1191890118@qq.com>
2023-03-07 12:18:40 +0800

    Init project

---
+++ ddnspod.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Import ardnspod functions
+. /etc/profile
+
+cur_dir=$(dirname "$(readlink -f "$0")")
+. ${cur_dir}/ardnspod
+
+# Combine your token ID and token together as follows
+arToken="$DDNSToken"
+
+# Web endpoint to be used for querying the public IPv6 address
+# Set this to override the default url provided by ardnspod
+# arIp6QueryUrl="https://6.ipw.cn"
+
+# Return code when the last record IP is same as current host IP
+# Set this to a value other than 0 to distinguish with a successful ddns update
+# arErrCodeUnchanged=0
+
+# Place each domain you want to check as follows
+# you can have multiple arDdnsCheck blocks
+
+# IPv4:
+arDdnsCheck "$DDNSURL" "$DDNSDomain"
